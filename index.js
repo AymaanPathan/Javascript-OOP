fb227775d37d5527ad02961e249b1efb11f3dc90
Aymaan <aymaanpathan5@gmail.com>
2024-10-12 12:00:24 +0530

    bad object [shallow copy]

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hi");
+const player = {
+  name: "?",
+  hp: 100,
+  mp: 0,
+  items: [],
+};
+
+const kratos = player;
+kratos.name = "kratos";
+kratos.hp = 90;
+
+const zeus = player;
+
+console.log(kratos);
+console.log(zeus);
