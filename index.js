270a3e5fda71b62f3370fb91abf943f6d8569c1f
Aymaan <aymaanpathan5@gmail.com>
2024-10-13 00:01:36 +0530

    Classes & Object Excercise - 2

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-class Animal {
-  constructor(type, name) {
-    this.type = type;
+class Student {
+  constructor(name, major, grades) {
     this.name = name;
+    this.major = major;
+    this.grades = grades;
+  }
+
+  addGrade(grade) {
+    this.grades.push(grade);
+  }
+  gpa() {
+    let gpa = 0;
+    for (let i = 0; i < this.grades.length; i++) {
+      gpa += this.grades[i];
+    }
+    return gpa / this.grades.length;
   }
 }
-const Tiger = new Animal("Tiger", "Robby");
-Tiger.name = "Manny";
-console.log(Tiger);
+
+const Eva = new Student("Eva", "Arts", [95, 75, 83]);
+Eva.addGrade(90);
+Eva.addGrade(12);
+console.log(Eva);
+console.log(Eva.gpa());
