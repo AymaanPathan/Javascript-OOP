ad3e96c05493da6f2e663420606a7ae3aa0ee950
Aymaan <aymaanpathan5@gmail.com>
2024-10-12 12:28:12 +0530

    correct way to create Player using class

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-class player {}
-
-const kratos = new player(); // correct way to differentiate with normal object and says this an an specific player object
-console.log(kratos);
-console.log({}); // this is a normal object we can't say this is a player object its just a simple normal object
-
-// other examples
-// 1. is car class -> is a specific objects of all cars
-// on the other hand when we create a simple object for cars we can't tag that object as a car and there is also a problem of shallow and depp copy
-// and also same work with function that returns a obj
+class Player {
+  constructor(name, hp, mp, items) {
+    // is works like a regular function called method
+    this.name = name; // this.name will works like Player.name
+    this.hp = hp;
+    this.mp = mp;
+    this.items = items;
+  }
+}
+const Kratos = new Player("Kratos", 90, 12, ["Blades"]);
+console.log(Kratos);
